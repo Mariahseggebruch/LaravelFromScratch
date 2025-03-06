3da29ebc0e663908ce86ce3aa2f60d32628f5a8e
Mariah Bargas <mariahbargas@Mariahs-MacBook-Pro.local>
2025-03-06 10:55:53 -0600

    Created about, index, and services page with minimal content and styling.

--- my-laravel8-app/routes/web.php
+++ my-laravel8-app/routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/users/{id}', function($id){
+//     return 'This is user '.id;
+// });
+// Dynamic value / parameter 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function(){
+    return view('pages.about');
+});
+
+Route::get('/', 'PagesController@index');
