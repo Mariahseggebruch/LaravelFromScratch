178a50fc5703aef1ab5217894b6648af018428d3
Mariah Bargas <mariahbargas@Mariahs-MacBook-Pro.local>
2025-03-10 09:58:54 -0500

    End of Laravel From Scratch video 9, User Auth

--- my-laravel8-app/resources/views/inc/navbar.blade.php
+++ my-laravel8-app/resources/views/inc/navbar.blade.php
@@ -31,11 +31,10 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="/posts/create">Create Post</a></li>
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    {{-- <li><a href="{{ route('login') }}">Login</a></li> --}} 
-                    {{-- <li><a href="{{ route('register') }}">Register</a></li> --}}
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
